de210c69717d913ae55b54aba972b3ca696c79cb
asmitapaul22 <asmitapaul2000@gmail.com>
2020-08-25 11:17:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practice9';
+  public ngOnInit(){
+    $(document).ready(function(){
+      $("button").click(function(){
+        var div=$("div");
+        div.animate({left:"1000px"},"slow");
+      div.animate({fontSize:"5em"},"slow");
+       });
+    });
+
+  }
 }
